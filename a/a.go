27979a719e314f531b7890d2a992d20d6a45398e
BloodyRainer <rambo.ramon.de@gmail.com>
2018-11-06 22:32:02 +0100

    add package a

---
+++ a/a.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+	"github.com/BloodyRainer/submodules/b"
+)
+
+const Name = b.Name
+
+func main() {
+	fmt.Println(Name)
+}
---
+++ a/go.mod
@@ -0,0 +1,3 @@
+module github.com/BloodyRainer/submodules/a
+
+require github.com/BloodyRainer/submodules/b v0.1.1
